714daef0ebdc137bc2b44b500dd2e9c21db299b5
kuanglian1000 <kuanglian1000@gmail.com>
2021-01-11 17:31:43 +0800

    2011-01-11 下午進度

---
+++ List.py
@@ -0,0 +1,7 @@
+# Use a range of indexes to print the third, fourth, and fifth item in the list.
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+# the number of items in the list
+print(len(fruits))
--- StringFormat.py
+++ StringFormat.py
@@ -1,3 +1,36 @@
+print(bool("abc")) # return True
+print(bool(0)) # return False
+
+# Get the characters from index 2 to index 4 (llo).
+txt = "Hello World"
+x = txt[2:5]
+print(x)
+
+fruits = ["apple" , "banana"]
+if "apple"  in fruits:
+  print("Yes , apple is a fruit.")
+
+if 1 and 0:
+  print(1)
+else:
+  print(0)
+
+# 去除字串前後空白值
+txt = " Hello World "
+print(txt.strip())
+
+# 字串轉大寫
+txt = " Hello World "
+print(txt.upper())
+
+# 字串轉小寫
+txt = " Hello World "
+print(txt.lower())
+
+# 字串取代(replace H with K)
+txt = " Hello World "
+print(txt.replace("H","K"))
+
 # String format()
 price = 49
 txt = "The price is {} dollars"
